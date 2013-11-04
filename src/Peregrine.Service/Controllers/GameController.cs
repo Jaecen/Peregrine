c58cb49949c8756f6c32dabb637675f0a6db3c55
Jason Addington <jason@addington.com>
2013-11-03 19:18:12 -0800

    Add controller scaffolds. Remove several unneeded UI features. Add data project.

--- src/Peregrine.Service/Controllers/GameController.cs
+++ src/Peregrine.Service/Controllers/GameController.cs
@@ -4,20 +4,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Newtonsoft.Json.Linq;
 
 namespace Peregrine.Service.Controllers
 {
-    public class EventController : ApiController
+    public class GameController : ApiController
     {
-		public JObject Get()
-		{
-
-		}
-
-		public JObject Put()
-		{
-
-		}
     }
 }
