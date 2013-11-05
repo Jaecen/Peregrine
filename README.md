5842802d7697d86c0251a443b3907b598ec983f9
Jason Addington <jason@addington.com>
2013-11-04 22:03:34 -0800

    Implement player controller.

--- README.md
+++ README.md
@@ -4,30 +4,64 @@ Swiss-pairing and ranking management.
 
 ##API
 ###/tournament
-POST: Creates a new tournament.
+*	**POST**
 
-###/tournament/{key}
-GET: Gets a summary of the tournament.
+	Creates a new tournament. Returns a summary of the tournament with a "key" property. Use this value to access subresources of the tournament. This method will always succeed.
 
-###/tournament/{id}/player/{name}
-GET: Gets a summary of the player.
+###/tournament/*{key}*
+*	**GET**
 
-PUT: Adds a player to the tournament.
+	Returns a summary of the tournament.
 
-DELETE: Drops a player from the tournament.
+	Returns a 404 if no tournament with the provided key exists.
 
-###/tournament/{id}/round/{number}
-GET: Gets a summary of the round.
+###/tournament/*{key}*/player/*{name}*
+*	**GET** 
 
-###/tournament/{id}/round/{number}/game/{number}
-GET: Gets a summary of the game.
+	Returns a summary of the player.
 
-PUT: Sets the results of the game.
+	Returns a 404 if no tournament with the provided key exists.
 
-###/tournament/{id}/round/{number}/pairings
-GET: Gets the pairings for the round.
+	Returns a 404 if no player with the provided name exists.
 
-###/tournament/{id}/standings
-GET: Gets the standings for the tournament.
+*	**PUT**
+	
+	Adds a player to the tournament. Players can not be added after the first game result is entered.
+
+	Returns a 404 if no tournament with the provided key exists.
+
+	Returns a 409 if a player with the provided name already exists.
+
+*	**DELETE**
+
+	Drops a player from the tournament. If no game results have been recorded, this simply deletes the player from the tournament.
+
+	Returns a 404 if no tournament with the provided key exists.
+
+	Returns a 404 if no player with the provided name exists.
+
+###/tournament/*{key}*/round/*{number}*
+*	**GET**
+
+	Returns a summary of the round.
+
+###/tournament/*{key}*/round/*{number}*/game/*{number}*
+*	**GET**
+
+	Returns a summary of the game.
+
+*	**PUT**
+
+	Sets the results of the game. Results for a previous round can not be changed once a new round has started.
+
+###/tournament/*{key}*/round/*{number}*/pairings
+*	**GET**
+
+	Returns the pairings for the round.
+
+###/tournament/*{key}*/standings
+*	**GET**
+
+	Returns the current standings for the tournament.
 
 
--- src/Peregrine.Service/Controllers/PlayerController.cs
+++ src/Peregrine.Service/Controllers/PlayerController.cs
@@ -4,10 +4,115 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Peregrine.Data;
 
 namespace Peregrine.Service.Controllers
 {
+	[RoutePrefix("tournament/{key}")]
     public class PlayerController : ApiController
     {
+		[Route("players", Name = "Player.List")]
+		public IHttpActionResult Get(Guid key)
+		{
+			using(var dataContext = new DataContext())
+			{
+				var tournament = dataContext
+					.Tournaments
+					.FirstOrDefault(t => t.Key == key);
+
+				if(tournament == null)
+					return NotFound();
+
+				return Ok(new
+				{
+					Players = tournament
+						.Players
+						.ToArray()
+				});
+			}
+		}
+		
+		[Route("player/{name}", Name = "Player.Post")]
+		public IHttpActionResult Put(Guid key, string name)
+		{
+			using(var dataContext = new DataContext())
+			{
+				var tournament = dataContext
+					.Tournaments
+					.FirstOrDefault(t => t.Key == key);
+
+				if(tournament == null)
+					return NotFound();
+
+				if(tournament
+					.Players
+					.Where(p => p.Name == name)
+					.Any())
+					return Conflict();
+
+				var player = new Player
+				{
+					Name = name,
+				};
+
+				tournament.Players.Add(player);
+				dataContext.SaveChanges();
+
+				return CreatedAtRoute("Player.Get", new { key = tournament.Key, name = player.Name }, player);
+			}
+		}
+
+		[Route("player/{name}", Name = "Player.Get")]
+		public IHttpActionResult Get(Guid key, string name)
+		{
+			using(var dataContext = new DataContext())
+			{
+				var tournament = dataContext
+					.Tournaments
+					.FirstOrDefault(t => t.Key == key);
+
+				if(tournament == null)
+					return NotFound();
+
+				var player = tournament
+					.Players
+					.Where(p => p.Name == name)
+					.FirstOrDefault();
+
+				if(player == null)
+					return NotFound();
+
+				return Ok(player);
+			}
+		}
+
+		[Route("player/{name}", Name = "Player.Delete")]
+		public IHttpActionResult Delete(Guid key, string name)
+		{
+			using(var dataContext = new DataContext())
+			{
+				var tournament = dataContext
+					.Tournaments
+					.FirstOrDefault(t => t.Key == key);
+
+				if(tournament == null)
+					return NotFound();
+
+				var player = tournament
+					.Players
+					.Where(p => p.Name == name)
+					.FirstOrDefault();
+
+				if(player == null)
+					return NotFound();
+
+				//TODO: Implement drop vs. delete
+
+				tournament.Players.Remove(player);
+				dataContext.SaveChanges();
+
+				return Ok();
+			}
+		}
     }
 }
