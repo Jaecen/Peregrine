3a360db7780ea91a16ba9f091d886c2339b2b154
Jason Addington <jason@addington.com>
2014-03-28 00:02:01 -0700

    Improve debug display of players.

--- src/Peregrine.Data/Player.cs
+++ src/Peregrine.Data/Player.cs
@@ -7,5 +7,10 @@ namespace Peregrine.Data
 		public virtual int Id { get; set; }
 		public virtual string Name { get; set; }
 		public virtual bool Dropped { get; set; }
+
+		public override string ToString()
+		{
+			return Name;
+		}
 	}
 }
