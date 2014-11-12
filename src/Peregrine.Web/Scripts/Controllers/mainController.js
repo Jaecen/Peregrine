f1dce3a8261d3d1f15286afa55893a510430730d
scott <friendscottn@gmail.com>
2014-11-11 23:48:55 -0800

    Adding the ability to find a tournament by tournament code

--- src/Peregrine.Web/Scripts/Controllers/mainController.js
+++ src/Peregrine.Web/Scripts/Controllers/mainController.js
@@ -1,8 +1,8 @@
 ﻿angular
 .module('peregrineUi.controllers')
 .controller('mainController', [
-	'$scope', 'tournamentResource',
-	function($scope, tournamentResource) {
+	'$scope', '$location', 'tournamentResource',
+	function ($scope, $location, tournamentResource) {
 		$scope.error = '';
 		tournamentResource.query(
 			{},
@@ -13,5 +13,21 @@
 			function error() {
 				$scope.error = 'We were unable to retrieve the tournament list';
 			});
+
+		$scope.goToTournament = function () {
+			if ($scope.searchTournamentCode) {
+				tournamentResource.get({ tournamentKey: $scope.searchTournamentCode },
+				function success(tournament) {
+					//redirect to the tournament if it exists
+					$location.path('/tournamentedit/' + tournament.key);
+				},
+				function error() {
+					$scope.error = 'So sorry, but that is not a valid tournament code.';
+				});
+			}
+			else {
+				$scope.error = 'Please enter a tournament code';
+			}
+		};
 	}
 ]);
