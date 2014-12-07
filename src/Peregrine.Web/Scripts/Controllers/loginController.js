f346026a7f955674169a9ffcd5e35d08341b6196
frienscottn <friendscottn@gmail.com>
2014-12-07 15:07:33 -0800

    - Got basic log in and log out working - Moved style.css into theme-customizations.less

--- src/Peregrine.Web/Scripts/Controllers/loginController.js
+++ src/Peregrine.Web/Scripts/Controllers/loginController.js
@@ -1,37 +1,20 @@
 ﻿angular
 .module('peregrineUi.controllers')
 .controller('loginController', [
-	'$scope', '$http', '$location',
-	function ($scope, $http, $location) {
+	'$scope', '$http', '$location', '$rootScope',
+	function ($scope, $http, $location, $rootScope) {
 		$scope.error = '';
 
-		$scope.login = function () {
+		$scope.loginClick = function () {
 			if ($scope.loginEmail === undefined)
 				$scope.error = 'Whoops looks like you missed the email box there!';
 			if ($scope.loginPassword === undefined)
 				$scope.error = 'Yeah sorry we\'re gonna need your password on that.';
 
-			$http({
-				method: 'POST',
-				url: '/Token',
-				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-				transformRequest: function(obj) {
-					var str = [];
-					for(var p in obj)
-						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-					return str.join("&");
-				},
-				data: { grant_type: 'password', username: $scope.loginEmail, password: $scope.loginPassword }
-			})
-			.success(function (data, status, headers, config) {
-				console.log(data.access_token);
-				sessionStorage.setItem("access_token", data.access_token);
-			}).error(function (data, status, headers, config) {
-				$scope.error = status;
-			})
+			login($scope.loginEmail, $scope.loginPassword);
 		}
 
-		$scope.register = function (email, password) {
+		$scope.registerClick = function (email, password) {
 			if ($scope.registerEmail === undefined)
 				$scope.error = 'Whoops looks like you missed the email box there!';
 			if ($scope.registerPassword === undefined)
@@ -44,9 +27,33 @@
 				data: { email: $scope.registerEmail, password: $scope.registerPassword }
 			})
 			.success(function (data, status, headers, config) {
-				$location.path('/');
+				$scope.login($scope.registerEmail, $scope.registerPassword)
 			}).error(function (data, status, headers, config) {
 				$scope.error = data.ModelState[""][0];
+			})
+		}
+
+		function login(email, password){
+			$http({
+				method: 'POST',
+				url: '/Token',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				transformRequest: function (obj) {
+					var str = [];
+					for (var p in obj)
+						str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+					return str.join("&");
+				},
+				data: { grant_type: 'password', username: email, password: password }
+			})
+			.success(function (data, status, headers, config) {
+				sessionStorage.setItem("accessToken", data.access_token);
+				sessionStorage.setItem("userName", data.userName);
+				//tell the userlinks in the header to check for a login
+				$rootScope.$emit('login')
+				$location.path('/');
+			}).error(function (data, status, headers, config) {
+				$scope.error = status;
 			})
 		}
 	}
---
+++ src/Peregrine.Web/Scripts/Controllers/userLinksController.js
@@ -0,0 +1,36 @@
+﻿angular
+.module('peregrineUi.controllers')
+.controller('userLinksController', [
+	'$scope', '$http', '$rootScope',
+	function ($scope, $http, $rootScope) {
+
+		function updateLoginStatus() {
+			$scope.loggedIn = (sessionStorage.getItem('accessToken') != null && sessionStorage.getItem('accessToken').length > 0);
+			$scope.userName = sessionStorage.getItem('userName');
+		}
+
+		//check the login status on page load
+		updateLoginStatus();
+		
+		//listen for the login event from other controllers
+		$rootScope.$on('login', function () {
+			updateLoginStatus();
+		});
+
+		//logout
+		$scope.logout = function () {
+			$http({
+				method: 'POST',
+				url: '/api/account/logout',
+				headers: { 'Content-Type': 'application/json' }
+			})
+			.success(function (data, status, headers, config) {
+				sessionStorage.removeItem('accessToken');
+				sessionStorage.removeItem('userName');
+				updateLoginStatus();
+			}).error(function (data, status, headers, config) {
+				$scope.error = "We were unable to log you out";
+			})
+		}
+	}
+]);
